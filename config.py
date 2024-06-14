333d06c0cad527ea4da0573525e5e3a6b9eb3b9c
Yves IRAKOZE MFURA <132387277+yvesmfura@users.noreply.github.com>
2024-06-14 19:31:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask-Mail configuration
