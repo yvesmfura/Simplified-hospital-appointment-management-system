6c2769957349eeda7db5eb0503d1a1c89d915546
Yves IRAKOZE MFURA <132387277+yvesmfura@users.noreply.github.com>
2024-06-14 19:10:37 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'fg7rh849984bhdbd9477484gdbdbvxtsbxjdjd994474bbdvssjsk')
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///site.db')
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask-Mail configuration
-    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))  # Ensure to convert to the appropriate type (int, str, etc.)
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', True) in ['True', 'true', '1']  # Convert string to boolean if needed
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'your-email@example.com')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'your-email-password')
-    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', 'your-email@example.com')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower() in ['true', '1']
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
