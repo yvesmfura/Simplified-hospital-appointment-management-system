d44447d607a57d52cb3ef46c58670d212ac78d76
Yves IRAKOZE MFURA <132387277+yvesmfura@users.noreply.github.com>
2024-06-14 23:43:11 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    # Load the secret key from environment variables or use a default for development/testing
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')  # Replace 'default-secret-key' with a secure default for local testing
+    # Load the secret key from environment variables
+    SECRET_KEY = os.getenv('SECRET_KEY') 
 
-    # Load the database URI from environment variables or use SQLite for local development/testing
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI', 'sqlite:///site.db')
+    # Load the database URI from environment variables
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask-Mail configuration loaded from environment variables
