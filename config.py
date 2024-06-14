8e9921020cbb7502f523d00212761d08d0a9b045
Yves IRAKOZE MFURA <132387277+yvesmfura@users.noreply.github.com>
2024-06-14 20:09:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = "fg7rh849984bhdbd9477484gdbdbvxtsbxjdjd994474bbdvssjsk"
     SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Flask-Mail configuration
-    MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'true').lower() in ['true', '1']
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587  # Port for TLS
+    MAIL_USE_TLS = True  # Use TLS encryption
+    MAIL_USERNAME = 'shamssupport@gmail.com'  # email username
+    MAIL_PASSWORD = 'Software@2024'  # email password
+    MAIL_DEFAULT_SENDER = 'shamssupport@gmail.com'  # Default sender email
